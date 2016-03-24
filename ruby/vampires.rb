63d8b75eeb47382eb22aa60de1e5b0fbcee2cd68
Danny Bravman <dbravman@gmail.com>
2016-03-24 14:04:01 -0500

    release 3 complete

--- ruby/vampires.rb
+++ ruby/vampires.rb
@@ -13,49 +13,57 @@ def checkAge birthyear, age
 	return (age..age+1) === (Time.new.year - birthyear) 
 end
 
-# release 1
-puts "What is your name?"
-name = gets.chomp
-puts "How old are you?"
-age = gets.chomp.to_i
-puts "What year were you born?"
-year = gets.chomp.to_i
-puts "Our company cafeteria serves garlic bread.  Should we order some for you? (Y/N)"
-garlicbread = YNConvert gets.chomp
-puts "Would you like to enroll in the company's health insurance? (Y/N)"
-insurance = YNConvert gets.chomp
+# release 3
+puts "How many employees will be processed?"
+employeeNumber = gets.chomp.to_i
 
-#release 2
-ageCorrect = checkAge year, age
+employeeNumber.times do
 
-# this seems efficient, but not in the required order 
-if name == "Drake Cula" || name == "Tu Fang"
-	puts "Definitely a vampire."
-elsif ageCorrect && (garlicbread ^ insurance)
-	puts "Probably not a vampire."
-elsif !garlicbread ^ !insurance
-	puts "Probably a vampire."
-elsif !garlicbread && !insurance
-	puts "Almost certainly a vampire."
-else 
-	puts "Results inconclusive."
+	# release 1
+	puts "What is your name?"
+	name = gets.chomp
+	puts "How old are you?"
+	age = gets.chomp.to_i
+	puts "What year were you born?"
+	year = gets.chomp.to_i
+	puts "Our company cafeteria serves garlic bread.  Should we order some for you? (Y/N)"
+	garlicbread = YNConvert gets.chomp
+	puts "Would you like to enroll in the company's health insurance? (Y/N)"
+	insurance = YNConvert gets.chomp
+
+	#release 2
+	ageCorrect = checkAge year, age
+
+	# this seems efficient, but not in the required order 
+	if name == "Drake Cula" || name == "Tu Fang"
+		puts "Definitely a vampire."
+	elsif ageCorrect && (garlicbread ^ insurance)
+		puts "Probably not a vampire."
+	elsif !garlicbread ^ !insurance
+		puts "Probably a vampire."
+	elsif !garlicbread && !insurance
+		puts "Almost certainly a vampire."
+	else 
+		puts "Results inconclusive."
+	end
+
+	# # this seems inefficient, but satisfies "Match the following conditions in the order they're listed"
+	# result = nil
+	# if ageCorrect && (garlicbread ^ insurance)
+	# 	result = "Probably not a vampire."
+	# elsif !ageCorrect && (!garlicbread ^ !insurance)
+	# 	result = "Probably a vampire."
+	# elsif !ageCorrect && (!garlicbread && !insurance)
+	# 	result = "Almost certainly a vampire."
+	# end
+
+	# if name == "Drake Cula" || name == "Tu Fang"
+	# 	result = "Definitely a vampire."
+	# end
+
+	# if result == nil 
+	# 	result = "Results inconclusive."
+	# end
+	# puts result
 end
 
-# # this seems inefficient, but satisfies "Match the following conditions in the order they're listed"
-# result = nil
-# if ageCorrect && (garlicbread ^ insurance)
-# 	result = "Probably not a vampire."
-# elsif !ageCorrect && (!garlicbread ^ !insurance)
-# 	result = "Probably a vampire."
-# elsif !ageCorrect && (!garlicbread && !insurance)
-# 	result = "Almost certainly a vampire."
-# end
-
-# if name == "Drake Cula" || name == "Tu Fang"
-# 	result = "Definitely a vampire."
-# end
-
-# if result == nil 
-# 	result = "Results inconclusive."
-# end
-# puts result
