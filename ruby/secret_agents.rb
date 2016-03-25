88f5851a0b99eebc953f602253a6ffad87d34159
Danny Bravman <dbravman@gmail.com>
2016-03-25 14:30:43 -0500

    switched driver

---
+++ ruby/secret_agents.rb
@@ -0,0 +1,21 @@
+-encrypt system (maybe with .next) so it will advance every letter one letter 
+forward. lowercase/uppercase make no difference
+    #.next while/until loop ; also 
+    #    (1st attempt - .next)
+    #    password.next make equal to variable name so can be called
+    #    abc_string.index[whatever is next in index]
+
+        #encrption = password + abc_string
+
+        step 1: password[0].next = pass_var
+        step 2: while the password.index is <= password.length, puts pass_var, end
+            unable to delete so (step 3:) pass_var += encrpyt_pass = answer
+
+-decrypt reverses .next, no .reverse, use index to go backward one letter.
+    "abcdefghijklmnopqrstuvwxyz" = key
+
+    step 1: password[-1]
+        key.index(whatever the first letter is)
+
+    step 2: abc_string.index[whatever is next in index = abc_decrypt, 
+    step 3: answer = abc_decrypt +=
