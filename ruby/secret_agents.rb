657997398b30165ba5501706ab18bb8ee64e7ab1
Danny Bravman <dbravman@gmail.com>
2016-03-25 15:37:58 -0500

    releases 3 and 4 complete

--- ruby/secret_agents.rb
+++ ruby/secret_agents.rb
@@ -1,21 +1,55 @@
--encrypt system (maybe with .next) so it will advance every letter one letter 
-forward. lowercase/uppercase make no difference
-    #.next while/until loop ; also 
-    #    (1st attempt - .next)
-    #    password.next make equal to variable name so can be called
-    #    abc_string.index[whatever is next in index]
+#-encrypt system (maybe with .next) so it will advance every letter one letter 
+#forward. lowercase/uppercase make no difference
+# loop this while password is not nil
+#      
+#     step 1: password[0] = pass_var
+#     step 2: abc_string.index[whatever is after 2] = abc_encrypt
+#     step 3: pass_var += abc_encrypt    
 
-        #encrption = password + abc_string
+def encrypt (password)
+  # Uppercase and spaces will be ignored!
+  password.delete!(" ")
+  password.downcase!
+  encrypted_password = ""
+  while password != ""
+    pass_var = password[0].next
+    if pass_var == "aa"
+      pass_var = "a"
+    end
+    password = password[1..-1]
+    encrypted_password += pass_var
+  end
+  encrypted_password
+end
 
-        step 1: password[0].next = pass_var
-        step 2: while the password.index is <= password.length, puts pass_var, end
-            unable to delete so (step 3:) pass_var += encrpyt_pass = answer
 
--decrypt reverses .next, no .reverse, use index to go backward one letter.
-    "abcdefghijklmnopqrstuvwxyz" = key
 
-    step 1: password[-1]
-        key.index(whatever the first letter is)
+# -decrypt reverses .next, no .reverse, use index to go backward one letter.
+#     "abcdefghijklmnopqrstuvwxyz" = key
 
-    step 2: abc_string.index[whatever is next in index = abc_decrypt, 
-    step 3: answer = abc_decrypt +=
+#     step 1: password[0]
+#         key.index(whatever the first letter is)
+#     step 2: abc_string.index[whatever is next in index = abc_decrypt, 
+#     step 3: answer = abc_decrypt +=
+
+def decrypt (encrypted_password)
+  # Uppercase and spaces will be ignored!
+  encrypted_password.delete!(" ")
+  encrypted_password.downcase!
+  password = ""
+  key = "abcdefghijklmnopqrstuvwxyz"
+  while encrypted_password != ""
+    key_index = key.index(encrypted_password[0])
+    pass_var = key[key_index - 1]
+    encrypted_password = encrypted_password[1..-1]
+    password += pass_var
+  end
+  password
+end
+
+puts encrypt("abc")
+puts encrypt("zed")
+puts decrypt("bcd")
+puts decrypt("afe")
+
+puts decrypt(encrypt("The duck flies at midnight"))
