9faf975df92cdde8f90bf695e6282613da4921de
Danny Bravman <dbravman@gmail.com>
2016-04-06 15:14:45 -0500

    release1 done

--- ruby/solo.rb
+++ ruby/solo.rb
@@ -1,15 +1,47 @@
 # Fire-breathing dragon
+class Dragon
+
 # ATTRIBUTES:
 # name
 # age
 # demeanor
+	attr_reader :name
+	attr_accessor :age, :demeanor 
 #
 # METHODS:
-# breath_fire
+# initialize(name)
+	# name is supplied
+	# age is default a random number
+	# demeanor is default happy
+	def initialize(name)
+		@name = name
+		@age = 1 + rand(50)
+		@demeanor = "happy"
+	end
+
+# breathe_fire
 	# output text
+	def breathe_fire
+		puts "WHOOOOOOMPH! *sizzle*"
+	end
+
 # fly
 	# output text
+	def fly
+		puts "swoooosh! *flaps*"
+	end
+
 # meet_hero(demeanor)
+	def meet_hero(demeanor)
 	# IF demeanor is happy, output text
-	# ELSIF demeanor is unhappy, THEN breath_fire!
+		if @demeanor == "happy"
+			puts "Heya!  I'm a dragon!"
+	# ELSIF demeanor is angry, THEN breath_fire!
+		elsif @demeanor == "angry"
+			breathe_fire
 	# ELSE fly
+		else
+			fly
+		end
+	end
+end
