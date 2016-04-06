6e988f45434d24679d3799fcf537b6761542e943
Danny Bravman <dbravman@gmail.com>
2016-04-06 15:35:20 -0500

    release2 done

--- ruby/solo.rb
+++ ruby/solo.rb
@@ -33,7 +33,7 @@ class Dragon
 
 # meet_hero(demeanor)
 	def meet_hero(demeanor)
-	# IF demeanor is happy, output text
+	# IF demeanor is happy, THEN output text
 		if @demeanor == "happy"
 			puts "Heya!  I'm a dragon!"
 	# ELSIF demeanor is angry, THEN breath_fire!
@@ -45,3 +45,39 @@ class Dragon
 		end
 	end
 end
+
+# user interface
+def user_interface
+#•Your user should be allowed to create as many instances of your class as they like.
+	dragons = []
+	finished = false
+	until finished == true
+
+#•Prompt the user for each attribute, and don't forget to convert their input to the appropriate data type. 
+		puts "What is this dragon's name?"
+		name = gets.chomp
+		puts "How old is this dragon?"
+		age = gets.chomp.to_i
+		puts "What is this dragon's demeanor?"
+		demeanor = gets.chomp
+		
+	#Store these class instances in an array.
+		dragon = Dragon.new(name)
+		dragon.age = age
+		dragon.demeanor = demeanor
+		dragons << dragon
+
+		puts "Create another dragon? (y/n)"
+		finished = case gets.chomp
+			when "y" then false
+			when "n" then true
+		end
+	end
+
+#•When the user indicates that they are finished creating instances, loop through the array and print out the attributes of each instance as a confirmation message of what was created
+	dragons.each do |dragon| 
+		puts "#{dragon.name} is a #{dragon.age} year-old dragon who is feeling #{dragon.demeanor}."
+	end
+end
+
+user_interface
