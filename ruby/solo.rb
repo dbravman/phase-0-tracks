14876d596d6c9df0554fb0aa5844bd5782f122dd
Danny Bravman <dbravman@gmail.com>
2016-04-06 14:54:38 -0500

    release0 done

---
+++ ruby/solo.rb
@@ -0,0 +1,15 @@
+# Fire-breathing dragon
+# ATTRIBUTES:
+# name
+# age
+# demeanor
+#
+# METHODS:
+# breath_fire
+	# output text
+# fly
+	# output text
+# meet_hero(demeanor)
+	# IF demeanor is happy, output text
+	# ELSIF demeanor is unhappy, THEN breath_fire!
+	# ELSE fly
