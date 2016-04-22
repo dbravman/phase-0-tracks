e4e4dc910c61a8d25364b3ae04b8ee0762f2f24d
Danny Bravman <dbravman@gmail.com>
2016-04-22 14:41:21 -0500

    tables created

--- databases/guestlist/guestlist.rb
+++ databases/guestlist/guestlist.rb
@@ -27,3 +27,39 @@ class Event
 	end
 end
 
+def create_tables(db)
+	create_people_table_cmd = <<-SQL
+		CREATE TABLE IF NOT EXISTS people(
+			person_id INTEGER PRIMARY KEY,
+			first_name VARCHAR(255),
+			last_name VARCHAR(255)
+		)
+	SQL
+
+	create_events_table_cmd = <<-SQL
+		CREATE TABLE IF NOT EXISTS events(
+			event_id INTEGER PRIMARY KEY,
+			event VARCHAR(255),
+			date VARCHAR(255),
+			time VARCHAR(255)
+		)
+	SQL
+
+	create_rsvps_table_cmd = <<-SQL
+		CREATE TABLE IF NOT EXISTS rsvps(
+			rsvp_id INTEGER PRIMARY KEY,
+			attending BOOLEAN,
+			person_id INT,
+			event_id INT,
+			FOREIGN KEY(person_id) REFERENCES people.person_id,
+			FOREIGN KEY(event_id) REFERENCES events.person_id,
+		)
+	SQL
+
+	db.execute(create_people_table_cmd)
+	db.execute(create_events_table_cmd)
+	db.execute(create_rsvps_table_cmd)
+end
+
+#driver code
+create_tables(db)
