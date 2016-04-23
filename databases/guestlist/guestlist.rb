10378980a14f5f2f067ca5fb268a14ecf002a7ad
Danny Bravman <dbravman@gmail.com>
2016-04-22 21:02:12 -0500

    all tables have random data

--- databases/guestlist/guestlist.rb
+++ databases/guestlist/guestlist.rb
@@ -1,31 +1,8 @@
 require "sqlite3"
+require "faker"
 
 db = SQLite3::Database.new("guestlist.db")
 db.results_as_hash = true
-
-class Guest
-	attr_reader :first_name, :last_name
-
-	def initialize(first_name, last_name)
-		@first_name = first_name
-		@last_name = last_name
-	end
-end
-
-class Event
-	attr_reader :name, :date, :time
-
-	def initialize(name, date, time)
-		@name = name
-		@date = date
-		@time = time
-	end
-
-	def reschedule(new_date, new_time)
-		@date = new_date
-		@time = new_time
-	end
-end
 
 def create_tables(db)
 	create_people_table_cmd = <<-SQL
@@ -48,7 +25,7 @@ def create_tables(db)
 	create_rsvps_table_cmd = <<-SQL
 		CREATE TABLE IF NOT EXISTS rsvps(
 			rsvp_id INTEGER PRIMARY KEY,
-			attending VARCHAR(255),
+			attending BOOLEAN,
 			person_id INT,
 			event_id INT,
 			FOREIGN KEY(person_id) REFERENCES people(person_id),
@@ -61,7 +38,75 @@ def create_tables(db)
 	db.execute(create_rsvps_table_cmd)
 end
 
+def add_fake_data(db)
+	10.times do 
+		add_person(db, Faker::Name.first_name, Faker::Name.last_name)
+	end
+
+	3.times do
+		random = Random.new
+
+		#can't get Faker::Date or ::Time to work so doing this instead
+
+		date = 5.to_s + "/" + (random.rand(31)+1).to_s + "/16"
+
+		time = random.rand(12).to_s
+		ampm = case random.rand(2)
+		when 0 then "am"
+		when 1 then "pm"
+		end
+
+		add_event(db, Faker::Lorem.sentence(2), date, time+ampm)
+	end
+end
+
+def add_fake_rsvps(db, people, events)
+	people.each do |person|
+		person_id = person["person_id"]
+
+		events.each do |event|
+			event_id = event["event_id"]
+
+			attending = case Random.new.rand(2)
+			when 0 then true.to_s
+			when 1 then false.to_s
+			end
+
+			db.execute("INSERT INTO rsvps (attending, person_id, event_id) VALUES (?, ?, ?)", [attending, person_id, event_id])
+		end
+	end
+end
+
+
+def add_event(db, event, date, time)
+	db.execute("INSERT INTO events (event, date, time) VALUES (?, ?, ?)", [event, date, time])
+end
+
+def add_person(db, first_name, last_name)
+	db.execute("INSERT INTO people (first_name, last_name) VALUES (?, ?)", [first_name, last_name])
+end
+
+def add_rsvp(db, attending, person_id, event_id)
+	db.execute("INSERT INTO rsvps (attending, person_id, event_id) VALUES (?, ?, ?)", [attending, person_id, event_id])
+end
+
+
 #driver code
-create_tables(db)
+puts "Create new tables with fake data? (y/n)"
+restart = gets.chomp
+if restart == "y"
+	db.execute("DROP TABLE IF EXISTS people")
+	db.execute("DROP TABLE IF EXISTS events")
+	db.execute("DROP TABLE IF EXISTS rsvps")
+	create_tables(db)
+	add_fake_data(db)
+
+	people = db.execute("SELECT * FROM people")
+	events = db.execute("SELECT * FROM events")
+
+	add_fake_rsvps(db, people, events)
+	rsvps = db.execute("SELECT * FROM rsvps")
+end
 
 
+	
