1d58ecec0d35d6b6a308b254fff45a14f9e2d472
Danny Bravman <dbravman@gmail.com>
2016-04-28 23:53:03 -0500

    release 2 done

--- web_dev/client_side_js/script.js
+++ web_dev/client_side_js/script.js
@@ -1,3 +1,4 @@
+//release1
 //set the color of everything in els to blue
 function changeTextBlue() {
 	var els = document.getElementsByTagName("li");
@@ -8,3 +9,21 @@ function changeTextBlue() {
 }
 
 changeTextBlue();
+
+//release2
+
+//set the color of everything in els to blue
+function changeTextBlue(event) {
+    console.log(event);
+//   event.target = document.getElementsByTagName("li");
+   
+//    for (var i = 0; i < event.target.length; i++) {
+   event.target.style.color = "blue";
+   
+}
+
+//changeTextBlue();
+
+var text = document.getElementsByTagName("li");
+text[0].addEventListener("click", changeTextBlue);
+text[1].addEventListener("click", changeTextBlue);
